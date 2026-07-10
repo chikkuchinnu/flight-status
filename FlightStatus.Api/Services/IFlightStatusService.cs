01eb33dfc8c19e980de856eb19849d29e374fe22
chikkuchinnu <manikandancruss@gmail.com>
2026-07-10 13:37:13 +0530

    Update NormalizeStatus method return type

--- FlightStatus.Api/Services/IFlightStatusService.cs
+++ FlightStatus.Api/Services/IFlightStatusService.cs
@@ -5,6 +5,6 @@ namespace FlightStatus.Api.Services;
 public interface IFlightStatusService
 {
     Task<FlightStatusResult> GetFlightStatusAsync(string flightNumber, string date);
-    FlightStatus NormalizeStatus(string? providerStatus, string providerName);
+    FlightStatus.Api.Models.FlightStatus NormalizeStatus(string? providerStatus, string providerName);
     FlightStatusResult MergeResponses(ProviderResponse? aeroTrack, ProviderResponse? quickFlight);
 }
